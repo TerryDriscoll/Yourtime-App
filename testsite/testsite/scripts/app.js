658f7718d5132fd04e4c2ccdd6c30a30230ecdb2
TerryDriscoll <renley@btconnect.com>
2015-06-26 15:40:26 +0100

    yourtime commit

---
+++ testsite/testsite/scripts/app.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+	//grid gallery scroll
+	new GridScrollFx( document.getElementById( 'grid' ), {
+                viewportFactor : 0.4
+            } );
+	//search expand
+	new UISearch( document.getElementById( 'sb-search' ) );
+
+});
